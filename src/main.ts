cbae92f5fa0660d9adb43d4a3e243c97cb19dcad
HieuCuteDangYeu <nguyendinhhieu050505@gmail.com>
2025-10-18 17:10:50 +0700

    reorder CORS position

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,18 @@ import { setupSwagger } from './config/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CORS
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS?.split(',') || [
+      'http://localhost:5173',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  });
+
   // Security
   app.use(
     helmet({
@@ -32,17 +44,6 @@ async function bootstrap() {
 
   // Compression
   app.use(compression());
-
-  // CORS
-  app.enableCors({
-    origin: process.env.ALLOWED_ORIGINS?.split(',') || [
-      'http://localhost:5173',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    preflightContinue: false,
-  });
 
   // Global prefix
   app.setGlobalPrefix(process.env.API_PREFIX || 'api');
