2a6792742bce254fb9b3dd4ddce539e704ce8308
Nguyen Tan Huy <huynguyen2662005@gmail.com>
2025-10-19 22:41:45 +0700

    TermCRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { HealthController } from './health/health.controller';
 import { AttendanceModule } from './modules/attendance/attendance.module';
 import { GuardianModule } from './modules/guardian/guardian.module';
 import { StaffModule } from './modules/staff/staff.module';
-
+import { TermModule } from './modules/term/term.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -47,6 +47,7 @@ import { StaffModule } from './modules/staff/staff.module';
     AttendanceModule,
     GuardianModule,
     StaffModule,
+    TermModule,
   ],
   controllers: [HealthController],
   providers: [],
