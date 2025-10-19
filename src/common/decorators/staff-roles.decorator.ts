16ea1458d377df437f9fbba736ff24fba8ff5fd9
HieuCuteDangYeu <nguyendinhhieu050505@gmail.com>
2025-10-19 20:06:40 +0700

    update paths

--- src/common/decorators/staff-roles.decorator.ts
+++ src/common/decorators/staff-roles.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
-import { StaffRole } from '../enums/roles.enums';
+import { StaffRole } from '../enums/roles.enum';
 
 export const STAFF_ROLES_KEY = 'staff_roles';
 export const StaffRoles = (...roles: StaffRole[]) =>
